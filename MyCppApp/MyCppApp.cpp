ffc041f74a5abfb1e5fa80ad2fce667f6a0e05b9
Marc Goodner <mgoodner@microsoft.com>
2022-08-26 16:23:13 -0700

    First pass adding CMake Presets project for dev container

---
+++ MyCppApp/MyCppApp.cpp
@@ -0,0 +1,12 @@
+﻿// MyCppApp.cpp : Defines the entry point for the application.
+//
+
+#include "MyCppApp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello from inside a devcontainer." << endl;
+	return 0;
+}
